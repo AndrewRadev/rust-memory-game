3925f899230915c05224024471ec29d408fc5812
Andrew Radev <andrey.radev@gmail.com>
2020-12-30 14:24:11 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use ggez::{
+    Context,
+    ContextBuilder,
+    GameResult,
+    conf::{Conf, WindowMode},
+    event,
+    filesystem,
+    graphics,
+    timer,
+};
+
+use std::env;
+use std::path;
+
+#[derive(Debug)]
+struct MainState {
+    conf: Conf,
+}
+
+impl MainState {
+    fn new(_ctx: &mut Context, conf: Conf) -> GameResult<MainState> {
+        Ok(MainState { conf })
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        const DESIRED_FPS: u32 = 60;
+
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            let _seconds = 1.0 / (DESIRED_FPS as f32);
+
+            // TODO
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let dark_blue = graphics::Color::from_rgb(26, 51, 77);
+        graphics::clear(ctx, dark_blue);
+        graphics::present(ctx)?;
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let conf = Conf::new().
+        window_mode(WindowMode {
+            min_width: 1024.0,
+            min_height: 768.0,
+            ..Default::default()
+        });
+    let (mut ctx, event_loop) = ContextBuilder::new("memory-game", "Andrew").
+        conf(conf.clone()).
+        build().
+        unwrap();
+
+    // We add the CARGO_MANIFEST_DIR/resources do the filesystems paths so
+    // we we look in the cargo project for files.
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        filesystem::mount(&mut ctx, &path, true);
+    }
+
+    let state = MainState::new(&mut ctx, conf.clone()).unwrap();
+
+    event::run(ctx, event_loop, state);
+}
